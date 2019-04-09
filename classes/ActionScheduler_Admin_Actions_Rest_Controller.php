952445fb73839b58669d5f747509fff17abb8a07
Ron Rennick <ron@ronandandrea.com>
2019-04-09 11:51:23 -0300

    Populate REST endpoint with pending actions
    - Change the endpoint to be ASA specific. When merged into AS, the root can be changed to /as/ to avoid conflict
    - Use local time zone set in WP settings to be more human friendly
    - Sort by optional group or hook + scheduled date (vs creation/modified dates)

--- classes/ActionScheduler_Admin_Actions_Rest_Controller.php
+++ classes/ActionScheduler_Admin_Actions_Rest_Controller.php
@@ -17,14 +17,14 @@ class ActionScheduler_Admin_Actions_Rest_Controller extends WC_REST_CRUD_Control
 	 *
 	 * @var string
 	 */
-	protected $namespace = 'wc/v4';
+	protected $namespace = 'asa/v1';
 
 	/**
 	 * Route base.
 	 *
 	 * @var string
 	 */
-	protected $rest_base = 'reports/scheduled-actions';
+	protected $rest_base = 'actions';
 
 	/**
 	 * Rest controller instance.
@@ -32,6 +32,13 @@ class ActionScheduler_Admin_Actions_Rest_Controller extends WC_REST_CRUD_Control
 	 * @var ActionScheduler_Actions_Rest_Controller
 	  */
 	private static $instance = NULL;
+
+	/**
+	 * Array of seconds for common time periods, like week or month, alongside an internationalised string representation, i.e. "Day" or "Days"
+	 *
+	 * @var array
+	 */
+	private static $time_periods;
 
 	/**
 	 * Create an instance.
@@ -44,26 +51,200 @@ class ActionScheduler_Admin_Actions_Rest_Controller extends WC_REST_CRUD_Control
 		return self::$instance;
 	}
 
-	/**
-	 * Register routes.
-	 *
-	 */
-	public function register_routes() {
-		register_rest_route( $this->namespace, '/' . $this->rest_base, array(
-			array(
-				'methods'             => WP_REST_Server::READABLE,
-				'callback'            => array( $this, 'get_actions' ),
-			),
-		) );
+	function __construct() {
+		self::$time_periods = [
+			[
+				'seconds' => YEAR_IN_SECONDS,
+				'names'   => _n_noop( '%s year', '%s years', 'action-scheduler-admin' ),
+			],
+			[
+				'seconds' => MONTH_IN_SECONDS,
+				'names'   => _n_noop( '%s month', '%s months', 'action-scheduler-admin' ),
+			],
+			[
+				'seconds' => WEEK_IN_SECONDS,
+				'names'   => _n_noop( '%s week', '%s weeks', 'action-scheduler-admin' ),
+			],
+			[
+				'seconds' => DAY_IN_SECONDS,
+				'names'   => _n_noop( '%s day', '%s days', 'action-scheduler-admin' ),
+			],
+			[
+				'seconds' => HOUR_IN_SECONDS,
+				'names'   => _n_noop( '%s hour', '%s hours', 'action-scheduler-admin' ),
+			],
+			[
+				'seconds' => MINUTE_IN_SECONDS,
+				'names'   => _n_noop( '%s minute', '%s minutes', 'action-scheduler-admin' ),
+			],
+			[
+				'seconds' => 1,
+				'names'   => _n_noop( '%s second', '%s seconds', 'action-scheduler-admin' ),
+			],
+		];
 	}
 
 	/**
-	 * Return a list of actions matching the query args
+	 * Register routes.
+	 */
+	public function register_routes() {
+		register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base,
+			[
+				[
+					'methods'  => WP_REST_Server::READABLE,
+					'callback' => array( self::$instance, 'get_actions' ),
+				],
+			]
+		);
+	}
+
+	/**
+	 * Maps query arguments from the REST request.
+	 *
+	 * @param  WP_REST_Request $request Request array.
+	 * @return array
+	 */
+	protected function prepare_actions_query( $request ) {
+		$args = [
+			'offset'   => $request[ 'offset' ],
+			'per_page' => $request[ 'page' ],
+			'group'    => $request[ 'group' ],
+			'status'   => ActionScheduler_Store::STATUS_PENDING,
+			'orderby'  => strtolower( $request[ 'orderby' ] ),
+		];
+
+		$args[ 'orderby' ] = in_array( $args[ 'orderby' ], [ 'hook', 'group' ], true ) ? $args[ 'orderby' ] : '';
+
+		return $args;
+	}
+
+	/**
+	 * Return a list of actions matching the query args. Scheduled date is date used for sorting.
 	 *
 	 * @return array Of WP_Error or WP_REST_Response.
 	 */
 	public function get_actions( $request ) {
-		return rest_ensure_response( [] );
+		$args     = $this->prepare_actions_query( $request );
+		$actions  = as_get_scheduled_actions( $args );
+		$timezone = new DateTimeZone( get_option( 'timezone_string', 'UTC' ) );
+		$data     = [];
+
+		foreach ( $actions as $action_id => $action ) {
+			// Display schedule date in more human friendly WP configured time zone.
+			$next = $action->get_schedule()->next();
+			$next->setTimezone( $timezone );
+			$schedule    = new ActionScheduler_SimpleSchedule( $next );
+
+			$action_data = [
+				'id'         => $action_id,
+				'hook'       => $action->get_hook(),
+				'group'      => $action->get_group(),
+				'scheduled'  => $this->get_schedule_display_string( $schedule ),
+				'arguments'  => $action->get_args(),
+				'recurrence' => $this->get_recurrence( $action ),
+			];
+
+			$key          = ( ! empty( $args[ 'orderby' ] ) ? $action_data[ $args[ 'orderby' ] ] . '_' : '' ) . $next->getTimestamp();
+			$data[ $key ] = $action_data;
+		}
+
+		if ( 'DESC' === strtoupper( $request['order'] ) ) {
+			krsort( $data );
+		} else {
+			ksort( $data );
+		}
+
+		// Reset the array keys to prevent downstream key sorting.
+		return rest_ensure_response( array_values( $data ) );
 	}
 
+	/**
+	 * Convert an interval of seconds into a two part human friendly string.
+	 *
+	 * The WordPress human_time_diff() function only calculates the time difference to one degree, meaning
+	 * even if an action is 1 day and 11 hours away, it will display "1 day". This function goes one step
+	 * further to display two degrees of accuracy.
+	 *
+	 * Inspired by the Crontrol::interval() function by Edward Dale: https://wordpress.org/plugins/wp-crontrol/
+	 *
+	 * @param int $interval A interval in seconds.
+	 * @param int $periods_to_include Depth of time periods to include, e.g. for an interval of 70, and $periods_to_include of 2, both minutes and seconds would be included. With a value of 1, only minutes would be included.
+	 * @return string A human friendly string representation of the interval.
+	 */
+	private static function human_interval( $interval, $periods_to_include = 2 ) {
+
+		if ( $interval <= 0 ) {
+			return __( 'Now!', 'action-scheduler' );
+		}
+
+		$output = '';
+
+		for ( $time_period_index = 0, $periods_included = 0, $seconds_remaining = $interval; $time_period_index < count( self::$time_periods ) && $seconds_remaining > 0 && $periods_included < $periods_to_include; $time_period_index++ ) {
+
+			$periods_in_interval = floor( $seconds_remaining / self::$time_periods[ $time_period_index ]['seconds'] );
+
+			if ( $periods_in_interval > 0 ) {
+				if ( ! empty( $output ) ) {
+					$output .= ' ';
+				}
+				$output .= sprintf( _n( self::$time_periods[ $time_period_index ]['names'][0], self::$time_periods[ $time_period_index ]['names'][1], $periods_in_interval, 'action-scheduler-admin' ), $periods_in_interval );
+				$seconds_remaining -= $periods_in_interval * self::$time_periods[ $time_period_index ]['seconds'];
+				$periods_included++;
+			}
+		}
+
+		return $output;
+	}
+
+	/**
+	 * Returns the recurrence of an action or 'Non-repeating'. The output is human readable.
+	 *
+	 * @param ActionScheduler_Action $action
+	 *
+	 * @return string
+	 */
+	protected function get_recurrence( $action ) {
+		$recurrence = $action->get_schedule();
+		if ( $recurrence->is_recurring() ) {
+			if ( method_exists( $recurrence, 'interval_in_seconds' ) ) {
+				return sprintf( __( 'Every %s', 'action-scheduler' ), self::human_interval( $recurrence->interval_in_seconds() ) );
+			}
+
+			if ( method_exists( $recurrence, 'get_recurrence' ) ) {
+				return sprintf( __( 'Cron %s', 'action-scheduler' ), $recurrence->get_recurrence() );
+			}
+		}
+
+		return __( 'Non-repeating', 'action-scheduler' );
+	}
+
+	/**
+	 * Get the scheduled date in a human friendly format.
+	 *
+	 * @param ActionScheduler_Schedule $schedule
+	 * @return string
+	 */
+	protected function get_schedule_display_string( ActionScheduler_Schedule $schedule ) {
+
+		$schedule_display_string = '';
+
+		if ( ! $schedule->next() ) {
+			return $schedule_display_string;
+		}
+
+		$next_timestamp = $schedule->next()->getTimestamp();
+
+		$schedule_display_string .= $schedule->next()->format( 'Y-m-d H:i:s O' );
+		$schedule_display_string .= '<br/>';
+
+		if ( gmdate( 'U' ) > $next_timestamp ) {
+			$schedule_display_string .= sprintf( __( ' (%s ago)', 'action-scheduler' ), self::human_interval( gmdate( 'U' ) - $next_timestamp ) );
+		} else {
+			$schedule_display_string .= sprintf( __( ' (%s)', 'action-scheduler' ), self::human_interval( $next_timestamp - gmdate( 'U' ) ) );
+		}
+
+		return $schedule_display_string;
+	}
 }
